a35136b14ad0aab8fb635a3d2cd9e77fa14655ce
Rudy Chaney <rudy@rudeluvdesigns.com>
2015-11-13 17:21:03 -0800

    build tests, travis, update example

---
+++ test/cli.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var queryMonster = require('../dist/query-monster');
+
+describe('Return Object', function() {
+  it('Should never return undefined', function() {
+    expect(queryMonster.crush()).to.not.be.an('undefined');
+  });
+  it('Should always return an object', function() {
+    expect(queryMonster.crush()).to.be.an('object');
+  });
+  it('Should always return .query as a string ', function() {
+    expect(queryMonster.crush().query).to.be.a('string');
+  });
+  it('Should contain .phrases', function() {
+    expect(queryMonster.crush().phrases).to.be.ok;
+  });
+  it('Should contain .terms', function() {
+    expect(queryMonster.crush().terms).to.be.ok;
+  });
+});
